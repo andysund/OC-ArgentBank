6e29de3f7e4fb6375105ba18b6924bb973b27fb0
enoamaore14120 <121623851+enoamaore14120@users.noreply.github.com>
2025-03-15 16:19:08 +0100

    Argent Bank avec objectif fit cover, revoir effet enccart en breakpoint 1024

--- bankingapp/src/components/Hero.jsx
+++ bankingapp/src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
         <p className="subtitle">No fees.</p>
         <p className="subtitle">No minimum deposit.</p>
         <p className="subtitle">High interest rates.</p>
-        <p className="text">Open a savings account with Argent Bank today!</p>
+        <p className="text">Open a savings account with <br></br>Argent Bank today!</p>
       </section>
     </div>
     </>
